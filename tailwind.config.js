e15833c753b3a109faccd1a4dc6cb3760acc7084
isradev <isradev.contact@gmail.com>
2022-11-08 12:34:25 +0100

    Commit 3 - end - NintendWind-Repo

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,15 @@ module.exports = {
       extend: {
         backgroundImage: theme => ({
           'wave-pattern': "url('../img/wave-white.png')",
-          'wave-pattern-dots': "url('../img/pattern-white-dots.png')"
-        })
+          'wave-pattern-dots': "url('../img/pattern-white-dots.png')",
+          'pattern-characters-red': "url('../img/pattern-characters-red.png')",
+          'character-mario': "url('../img/mario.png')",
+          'wave-pink': "url('../img/wave-pink.png')",
+          'yellow-dots': "url('../img/pattern-yellow-dots.png')"
+        }),
+        screens:{
+          'xsm': '320px',
+        }
       },
     },
     variants: {
